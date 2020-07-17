01466afc1caa1d3a0f13e4afb4c0f267f6126271
Tim Horist <TimelikeClosure@users.noreply.github.com>
2020-07-16 17:10:43 -0700

    complete quiz2

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -7,8 +7,6 @@
 // you think each value is. That is, add either `string_slice` or `string`
 // before the parentheses on each line. If you're right, it will compile!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ("blue");
-    ("red".to_string());
-    (String::from("hi"));
-    ("rust is fun!".to_owned());
-    ("nice weather".into());
-    (format!("Interpolation {}", "Station"));
-    (&String::from("abc")[0..1]);
-    ("  hello there ".trim());
-    ("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue"); // &str literal
+    string("red".to_string()); // &str method to convert to String
+    string(String::from("hi")); // String static method to convert to String
+    string("rust is fun!".to_owned()); // &str method to create an owned version, which happens to be a String
+    string_slice("nice weather".into()); string("nice weather".into()); // does both: .into() allows an implied conversion
+    string(format!("Interpolation {}", "Station")); // format!() produces a value which cannot be determined from slicing a single string, so a new String must be created
+    string_slice(&String::from("abc")[0..1]); // &str (literal) -> String (String::from()) -> &String (&) -> &str ([0..1])
+    string_slice("  hello there ".trim()); // .trim() can be determined from slicing a &str, so it produces a &str
+    string("Happy Monday!".to_string().replace("Mon", "Tues")); // .replace() produces a value which cannot be determined from slicing the String, so a new String must be created
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase()); // .to_lowercase() produces a value which cannot be determined from slicing the &str, so a new String must be created
 }
